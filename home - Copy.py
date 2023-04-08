b2be9d2fa50f9b7226f5586a8833b82ba9bcda48
MohammedMuzammil22 <mohamedmuzamil22@gmail.com>
2023-04-08 15:42:14 +0530

    home copy updated

--- home - Copy.py
+++ home - Copy.py
@@ -1,1 +1,13 @@
 '''contains updated 4 files'''
+print("hello")
+# second commit
+
+for i in range(10):
+    print(i)
+# 3rd commit
+print("hello")
+# second commit
+
+for i in range(10):
+    print(i)
+# 3rd commit
